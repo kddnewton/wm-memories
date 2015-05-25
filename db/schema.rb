19743951745f318a62f552aeec21911e52a1a6ec
Kevin Deisz <kevin.deisz@gmail.com>
2015-05-24 22:04:34 -0400

    updating procfile and schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150524210632) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stories", force: :cascade do |t|
     t.integer  "year"
     t.text     "body"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.decimal  "lat",        precision: 10, scale: 6
     t.decimal  "lng",        precision: 10, scale: 6
   end
