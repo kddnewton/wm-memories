880c81b0faeca6168b54e99bf82c0578e070e648
Kevin Deisz <kevin.deisz@gmail.com>
2016-05-08 23:10:13 -0400

    upgrade to rails 5.0.0.beta4 and to turbolink 5.0.0.beta

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Log warnings to standard error
+  # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
