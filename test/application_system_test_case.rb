a5256fc2934d9d6b0e7f3c011ed93a341456648a
Kevin Deisz <kevin.deisz@gmail.com>
2017-05-02 18:05:04 -0400

    Dont run system tests on travis

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driver = ENV['CI'] ? :firefox : :chrome
-  driven_by :selenium, using: driver, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
