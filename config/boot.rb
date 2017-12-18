857423c39914e7a71b8407d2055ff86d76436051
Kevin Deisz <kevin.deisz@gmail.com>
2017-12-18 16:47:10 -0500

    Bump dependencies and ruby version

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup'
-
-require 'vernacular'
-Vernacular.configure(&:give_me_all_the_things!)
