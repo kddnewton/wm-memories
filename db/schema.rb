bf20fecb0d5e985cee2eabff9f4e5b9e998b644b
Kevin Deisz <kevin.deisz@gmail.com>
2019-11-07 12:43:15 -0500

    Upgrade to latest Rails config settings

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# typed: strict
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_133008) do
+ActiveRecord::Schema.define(version: 2019_11_07_174232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
