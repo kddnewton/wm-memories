eb50362b1b374e30f7493e42f4650f157384e96c
Kevin Deisz <kevin.deisz@gmail.com>
2019-11-07 10:31:57 -0500

    Type checking for stories admin controller

--- app/controllers/admin/stories_controller.rb
+++ app/controllers/admin/stories_controller.rb
@@ -1,24 +1,29 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Admin
   class StoriesController < ApplicationController
+    extend T::Sig
+
     authenticate_admin
 
     # GET /admin/stories
+    sig { void }
     def index
-      @stories = Story.feed_ordered
+      @stories = T.let(Story.feed_ordered, T.nilable(Story::ActiveRecord_Relation))
     end
 
     # PATCH /admin/stories/:id/approve
+    sig { void }
     def approve
-      @story = Story.find(params[:id])
-      @story.approve!
+      @story = T.let(Story.find(params[:id]), T.nilable(Story))
+      T.must(@story).approve!
     end
 
     # GET /admin/stories/:id
+    sig { void }
     def show
-      @story = Story.find(params[:id])
+      @story = T.let(Story.find(params[:id]), T.nilable(Story))
       render 'stories/show'
     end
   end
